aff02dd6aecd02372f2b7dd6514cdfc7b29a5df4
Luke Morton <lukemorton.dev@gmail.com>
2017-01-12 23:11:05 +0000

    Rename pipe() to thread()

--- test/thread.test.js
+++ test/thread.test.js
@@ -1,31 +1,31 @@
 import test from 'ava'
-import { pipe } from '../lib'
+import { thread } from '../lib'
 
 test('call single function with arg', async (t) => {
   const fn = (x) => x
-  t.true(await pipe(true, fn))
+  t.true(await thread(true, fn))
 })
 
 test('call multiple functions with arg', async (t) => {
   const identity = (a) => a
   const addOne = (n) => n + 1
-  t.is(await pipe(1, identity, addOne), 2)
+  t.is(await thread(1, identity, addOne), 2)
 })
 
 test('call with promise constructor', async (t) => {
   const identityThen = (a) => Promise.resolve(a)
-  t.true(await pipe(true, identityThen))
+  t.true(await thread(true, identityThen))
 })
 
 test('call with promise as first arg', async (t) => {
   const promise = Promise.resolve(true)
   const identity = (a) => a
-  t.throws(pipe(promise, identity), 'You cannot pass a promise as the first argument of pipe()')
+  t.throws(thread(promise, identity), 'You cannot pass a promise as the first argument of thread()')
 })
 
 test('call with multiple promise constructors', async (t) => {
   const addOneEventually = (n) => Promise.resolve(n + 1)
-  t.is(await pipe(1, addOneEventually, addOneEventually), 3)
+  t.is(await thread(1, addOneEventually, addOneEventually), 3)
 })
 
 test('validation use case', async (t) => {
@@ -33,6 +33,6 @@ test('validation use case', async (t) => {
   const validate = (data) => data
   const create = async (data) => ({ ...data, id: 1 })
 
-  const user = await pipe({}, json, validate, create)
+  const user = await thread({}, json, validate, create)
   t.is(user.id, 1)
 })
